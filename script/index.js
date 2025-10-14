ed63fcaf80411441045118ab0309e967f4e3dd0e
Priti Saxena <saxenapriti1008@gmail.com>
2025-10-14 10:19:30 +0200

    updated index.html, index.js
    added operators using 2 variables with numbers

--- script/index.js
+++ script/index.js
@@ -9,6 +9,22 @@ console.log(`My name is ${name}`);
 console.log(`My age is ${age}`);
 console.log(`My favoutitecolor is ${favoutitecolor}`);
 
+//let students = 60;
 
+//students = 60+1;
+//students = students+1;
+//students = students-1;
+//students = students*2;
+//students = students/2;
+
+//console.log(students);
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Substraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division", num1 / num2);
 
 
