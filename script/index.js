874228f26cc672480ff2f81bd483f7c8682819a8
Priti Saxena <saxenapriti1008@gmail.com>
2025-10-14 10:04:26 +0200

    updated index.html, index.js
    added Hello World!
    added 3 variables

---
+++ script/index.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+
+let name = "priti";
+let age = 38;
+let favoutitecolor = "purple";
+
+console.log(`My name is ${name}`);
+console.log(`My age is ${age}`);
+console.log(`My favoutitecolor is ${favoutitecolor}`);
+
+
+
+
