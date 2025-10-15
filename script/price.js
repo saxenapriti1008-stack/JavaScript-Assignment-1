18980f9260558dc6c681800372f3a3d12257cc99
Priti Saxena <saxenapriti1008@gmail.com>
2025-10-15 13:56:35 +0200

    updated price.js
    Added all variables and their values in price.js file

--- script/price.js
+++ script/price.js
@@ -2,19 +2,37 @@
 
 let userInput = prompt("Enter a price tag (for example: $49.99):");
 
+console.log("User input:", userInput);
+
 // 2. Remove the dollar sign from the string
 
 let priceWithoutDollar = userInput.replace("$" , "");
+
+console.log("Price without dollar sign:", priceWithoutDollar);
 
 // 3. Convert the string to a number
 
 let price = parseFloat(priceWithoutDollar);
 
+console.log("Converted price (number):", price);
+
 // 4. Calculate the new price with 10% discount
 
 let discount = price * 0.10;
+
+console.log("Discount (10%):", discount);
+
+  // 5. Print out the new discounted price
+
 let newPrice = price - discount;
 
-// 5. Print out the new discounted price
+console.log("New discount price:", newPrice);
 
 console.log(`Your new price is: $${newPrice.toFixed(2)}`);
+
+
+
+
+
+
+
