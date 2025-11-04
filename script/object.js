d4f3660ecf657d95bdf91dbf7ccabc07ff96d6ce
Priti Saxena <saxenapriti1008@gmail.com>
2025-11-04 12:49:26 +0100

    updated object.js
    added ATM function

--- script/object.js
+++ script/object.js
@@ -74,3 +74,45 @@ account.withdrawal(200);
 account.withdrawal(2000); 
 account.deposit(-50);     
 account.exitAccount();
+
+function atm() {
+  let running = true;
+
+  while (running) {
+    const choice = prompt(`
+Welcome to the ATM!
+Please choose an option:
+1. View Account Name
+2. Check Balance
+3. Deposit Money
+4. Withdraw Money
+5. Exit
+`);
+
+switch (choice) {
+      case "1":
+        account.getAccountName();
+        break;
+      case "2":
+        account.getBalance();
+        break;
+      case "3":
+        const depositAmount = parseFloat(prompt("Enter amount to deposit:"));
+        account.deposit(depositAmount);
+        break;
+      case "4":
+        const withdrawAmount = parseFloat(prompt("Enter amount to withdraw:"));
+        account.withdrawal(withdrawAmount);
+        break;
+      case "5":
+        account.exitAccount();
+        running = false;
+        break;
+      default:
+        account.accountError("Invalid selection. Please choose 1–5.");
+    }
+  }
+}
+
+// Run the ATM
+atm();
