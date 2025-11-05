f1906e678a8672aca71c03cd3d59bb3883e7a6cf
Priti Saxena <saxenapriti1008@gmail.com>
2025-11-05 16:33:00 +0100

    updated practice.js
    Added function to list movies
    added alert and foreach method

--- script/practice.js
+++ script/practice.js
@@ -19,3 +19,24 @@ function addMovie() {
   movieLibrary.push(movie);
   alert(` Movie "${title}" by ${director} added to your list!`);
 }
+
+
+//  Function to list all movies
+
+function listMovies() {
+  if (movieLibrary.length === 0) {
+    alert("Your movie list is empty!");
+    return;
+  }
+
+  let movieList = " Your Movies:\n\n";
+  movieLibrary.forEach((movie, index) => {
+    movieList += `${index + 1}. ${movie.title} by ${movie.director} - ${
+      movie.isWatched ? " Watched" : " Not Watched"
+    }\n`;
+  });
+
+  alert(movieList);
+  console.log(movieLibrary); // Optional for debugging
+}
+
